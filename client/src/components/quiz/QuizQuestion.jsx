ff04220a66198f07e1162363191e7964b8442c8c
RyomenDev <aakashvatsh007@gmail.com>
2025-03-05 17:46:57 +0530

    styling-quiz

--- client/src/components/quiz/QuizQuestion.jsx
+++ client/src/components/quiz/QuizQuestion.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import { LabelWithBorder } from "../../utils";
+// import { LabelWithBorder } from "../../utils";
 
 const QuizQuestion = ({
   question,
@@ -10,31 +10,30 @@ const QuizQuestion = ({
 }) => {
   return (
     <div className="flex flex-col items-center justify-center w-full max-w-3xl p-4 sm:p-6 text-white gap-4">
-      {/* Highlighted Question Box with Glassmorphic Effect*/}
-      <div className="w-4/5 p-4 sm:p-6 backdrop-blur-lg bg-amber-950/40 rounded-full text-white text-lg sm:text-xl md:text-2xl font-semibold text-center">
+      {/* Highlighted Question Box with Glassmorphic Effect */}
+      <div className="w-full sm:w-4/5 p-4 sm:p-6 backdrop-blur-lg bg-amber-950/40 rounded-xl sm:rounded-full text-white text-base sm:text-lg md:text-2xl font-semibold text-center">
         {question}
       </div>
 
       {/* Options Grid */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 w-full mt-4">
-        {choices?.map((choice, index) => (
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 sm:gap-6 w-full mt-4">
+        {choices.map((choice, index) => (
           <button
             key={index}
-            className={`rounded-3xl text-lg sm:text-xl font-semibold transition-all duration-300 shadow-md px-6 py-3 ${
+            className={`w-full sm:w-auto rounded-xl sm:rounded-3xl text-base sm:text-lg md:text-xl font-semibold transition-all duration-300 shadow-md px-4 sm:px-6 py-2 sm:py-3 border-2 ${
               selectedAnswer === choice
-                ? "bg-amber-950/40 text-black"
-                : "hover:bg-amber-950/10 hover:text-black"
+                ? "bg-amber-950/40 text-black border-amber-900"
+                : "border-white hover:bg-amber-950/20 hover:text-black"
             }`}
             onClick={() => handleAnswerSelect(choice)}
           >
-            {/* {choice} */}
-            <LabelWithBorder text={choice} index={index} />
+            {index + 1} : {choice}
           </button>
         ))}
       </div>
 
       {/* Timer Display */}
-      <p className="backdrop-blur-lg bg-white/5 px-3 text-red-600 text-2xl">
+      <p className="w-full sm:w-auto text-center backdrop-blur-lg bg-white/10 px-3 py-1 rounded-md text-red-500 text-lg sm:text-2xl">
         Time Left: {timer} seconds
       </p>
     </div>
