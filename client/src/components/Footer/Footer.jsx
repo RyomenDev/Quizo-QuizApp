5ac8f57ce3534f4839259c85834bbe208a332d24
RyomenDev <aakashvatsh007@gmail.com>
2025-03-05 18:12:45 +0530

    header & Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -3,31 +3,32 @@ import emailLogo from "../../assets/icons8-mail-50.png";
 
 function Footer() {
   return (
-    <section className="footer bg-gradient-to-b from-[#743d2b] via-[#5C4033] to-[#573e3e] text-white py-4">
-      <div className="footer-container flex justify-center gap-4 ">
+    <footer className="bg-gradient-to-b from-[#743d2b] via-[#5C4033] to-[#573e3e] text-white py-4 px-4">
+      <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
         <a
           href="https://www.linkedin.com/in/akash-mishra-2b2348224"
           target="_blank"
           rel="noopener noreferrer"
-          className="footer-link hover:opacity-80"
+          className="hover:opacity-80"
         >
           <img
             src={linkedinLogo}
             alt="LinkedIn"
-            className="w-8 h-8 transition-all"
+            className="w-8 h-8 sm:w-10 sm:h-10 transition-all"
           />
         </a>
-        <a
-          href="mailto: ai.akash.mishra@gmail.com"
-          className="footer-link hover:opacity-80"
-        >
-          <img src={emailLogo} alt="Email" className="w-8 h-8 transition-all" />
+        <a href="mailto:ai.akash.mishra@gmail.com" className="hover:opacity-80">
+          <img
+            src={emailLogo}
+            alt="Email"
+            className="w-8 h-8 sm:w-10 sm:h-10 transition-all"
+          />
         </a>
       </div>
-      <div className="copy-container text-center text-sm opacity-70">
+      <div className="text-center text-xs sm:text-sm opacity-70 mt-2">
         &copy;2025 Made by Akash Mishra
       </div>
-    </section>
+    </footer>
   );
 }
 
--- client/src/components/Header/TopHeader.jsx
+++ client/src/components/Header/TopHeader.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import HeaderData from "../../Data/HeaderData.jsx";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { LogoutBtn, LoginButton, RegisterButton } from "../../utils";
+import { LogoutBtn, LoginButton } from "../../utils";
 
 const Header = () => {
   const navigate = useNavigate();
   const authStatus = useSelector((state) => state?.auth?.status);
   const userData = useSelector((state) => state?.auth?.userData);
-
   const userName = userData?.name;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const [isContactLanguageOpen, setIsContactLanguageOpen] = useState(false); // State to toggle contact and language visibility
   const { topHeader } = HeaderData;
   const { languages, logo, appName } = topHeader;
 
@@ -19,91 +18,78 @@ const Header = () => {
     navigate("/");
   };
 
-  const handleHamburgerClick = () => {
-    setIsContactLanguageOpen(!isContactLanguageOpen); // Toggle contact and language section visibility
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
   };
 
   return (
-    <header className="relative top-0 left-0 w-full z-50 flex flex-wrap justify-between items-center py-6 px-6 bg-gradient-to-b from-[#4E2A1E] via-[#5C4033] to-[#3B2F2F] text-white shadow-lg pl-12">
-      {" "}
-      {/*bg-gradient-to-b from-slate-900 to-slate-400*/}
+    <header className="relative top-0 left-0 w-full z-50 flex items-center justify-between bg-gradient-to-b from-[#4E2A1E] via-[#5C4033] to-[#3B2F2F] text-white py-4 px-6 shadow-lg">
       {/* Logo Section */}
       <div
-        className="flex items-center justify-start cursor-pointer gap-2 hover:shadow-lg rounded-lg transition-all duration-300"
+        className="flex items-center gap-2 cursor-pointer hover:shadow-lg rounded-lg transition-all duration-300"
         onClick={handleLogoClick}
       >
         <img
           src={logo}
           alt="HealthBridge"
-          className="w-auto h-12 md:h-16 transition-transform duration-300 hover:scale-105"
+          className="w-auto h-10 md:h-14 transition-transform duration-300 hover:scale-105"
         />
-        <div className="ml-2 text-2xl font-semibold text-white md:text-3xl hover:text-indigo-400 transition-all duration-300">
+        <div className="text-xl font-semibold md:text-2xl hover:text-indigo-400 transition-all duration-300">
           {appName}
         </div>
       </div>
+
       {/* Hamburger Icon (Mobile) */}
-      <div
-        className="md:hidden flex items-center"
-        onClick={handleHamburgerClick} // Show contact and language options on click
-      >
-        <button className="text-white focus:outline-none">
-          <span className="block w-6 h-1 bg-white mb-2"></span>
-          <span className="block w-6 h-1 bg-white mb-2"></span>
-          <span className="block w-6 h-1 bg-white mb-2"></span>
+      <div className="md:hidden flex items-center">
+        <button className="text-white focus:outline-none" onClick={toggleMenu}>
+          <span className="block w-6 h-1 bg-white mb-1"></span>
+          <span className="block w-6 h-1 bg-white mb-1"></span>
+          <span className="block w-6 h-1 bg-white"></span>
         </button>
       </div>
-      {/* Search Bar */}
-      <div className="w-full md:w-1/4 flex justify-center mb-4 md:mb-0">
-        <form className="relative w-full max-w-lg" action="#">
+
+      {/* Search Bar (Hidden on Small Screens) */}
+      <div className="hidden md:flex w-1/4 justify-center">
+        <form className="relative w-full max-w-lg">
           <input
-            id="search"
             type="text"
             className="w-full py-2 pl-4 pr-12 rounded-lg border border-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 bg-gray-800 text-white"
-            name="s"
-            autoComplete="off"
             placeholder="Search..."
-            aria-label="Search..."
           />
           <button
-            id="search-submit"
             type="submit"
             className="absolute right-2 top-2 text-indigo-500 hover:text-indigo-700"
-            title="Search"
-            aria-label="Search"
           ></button>
         </form>
       </div>
-      {/* Language & Authentication Section (Visible on Mobile when Hamburger is clicked) */}
+
+      {/* Authentication & Language Section */}
       <div
         className={`${
-          isContactLanguageOpen
-            ? "flex flex-col items-center space-y-6 border absolute top-16 right-0 w-1/4  bg-gradient-to-b from-slate-900 to-slate-400 shadow-lg rounded-2xl p-4 transition-all duration-300 ease-in-out "
+          isMenuOpen
+            ? "flex flex-col items-center absolute top-16 right-0 w-2/3 bg-gradient-to-b from-slate-900 to-slate-400 shadow-lg rounded-2xl p-4 transition-all duration-300 ease-in-out"
             : "hidden"
-        } md:flex md:relative md:top-0 md:bg-transparent md:shadow-none md:p-0 md:space-y-0 md:space-x-6 md:flex-row`}
+        } md:flex md:relative md:top-0 md:bg-transparent md:shadow-none md:p-0 md:space-x-6 md:flex-row`}
       >
         {/* Authentication Section */}
-        <div className="w-full text-center md:w-auto">
+        <div className="text-center">
           {authStatus ? (
-            <div className="flex flex-col items-center gap-2 md:flex-row md:gap-4">
-              <div className="text-lg font-semibold text-white">
+            <div className="flex flex-col items-center md:flex-row md:gap-4">
+              <span className="text-lg font-semibold text-white">
                 Hello, {userName}
-              </div>
+              </span>
               <LogoutBtn />
             </div>
           ) : (
-            <div className="flex flex-col gap-3 w-full items-center md:flex-row md:gap-4">
+            <div className="flex flex-col gap-3 items-center md:flex-row md:gap-4">
               <LoginButton />
-              {/* <RegisterButton /> */}
             </div>
           )}
         </div>
 
         {/* Language Selector */}
-        <div className="w-full text-center md:w-auto">
-          <select
-            className="bg-gray-800 border border-gray-600 rounded-md py-2 px-3 text-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500"
-            aria-label="Select Language"
-          >
+        <div className="text-center">
+          <select className="bg-gray-800 border border-gray-600 rounded-md py-2 px-3 text-white text-sm focus:outline-none focus:ring-2 focus:ring-indigo-500">
             {languages?.map((lang) => (
               <option key={lang.code} value={lang.code} className="text-black">
                 {lang?.name}
