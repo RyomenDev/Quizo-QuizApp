030ddd4ca7fd3fbfcc83bf571b96b3c6e74334d9
RyomenDev <aakashvatsh007@gmail.com>
2025-03-05 18:13:22 +0530

    style:Footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import emailLogo from "../../assets/icons8-mail-50.png";
 function Footer() {
   return (
     <footer className="bg-gradient-to-b from-[#743d2b] via-[#5C4033] to-[#573e3e] text-white py-4 px-4">
-      <div className="flex flex-col sm:flex-row items-center justify-center gap-4">
+      <div className="flex items-center justify-center gap-4">
         <a
           href="https://www.linkedin.com/in/akash-mishra-2b2348224"
           target="_blank"
